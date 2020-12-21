afc71e7acc999f11ff4c5e3aadb21f19648ac06f
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-21 21:31:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            List<int> integerlist = new List<int>()
+{
+1,2,3,4,5,6,7,8
+};
+            var methodsyntax = (from obj in integerlist
+                               where obj > 5
+                               select obj).Sum();
+            // var methodsyntax = integerlist.Where(obj => obj > 5);
+
+           // foreach (var item in methodsyntax)
+            //{
+                Console.WriteLine(methodsyntax);
+            
+        }
+    }
+}
