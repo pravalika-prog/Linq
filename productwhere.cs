be8722f129d348773d04c639b3757dfeaba4a39f
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-21 21:30:48 +0530

    Add files via upload

---
+++ productwhere.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace linq
+{
+    class productwhere
+    {
+        static void Main(string[] args)
+        {
+            IList<string> productList = new List<string>()
+            {
+                "Hard Disk",
+                "Monitor",
+                "SSD Disk",
+                "RAM",
+                "Processor",
+                "Bluetooth",
+                "Keyboard & Mouse"
+            };
+
+            var result = productList.Where(p => p.Contains("Disk"));
+
+            foreach (string str in result)
+            {
+                Console.WriteLine(str.ToString());
+            }
+
+        }
+    }
+}
