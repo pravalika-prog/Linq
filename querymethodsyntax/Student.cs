e23d156489e85ef85488253f966db5347a5807a9
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-21 21:30:11 +0530

    Add files via upload

---
+++ querymethodsyntax/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linq
+{
+  public  class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+public string gender { get; set; }
+        
+    }
+}
