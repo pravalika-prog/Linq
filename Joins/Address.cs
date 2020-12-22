a528b25867ef865327d16a3af682bfe27404d24b
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-22 22:24:40 +0530

    Add files via upload

---
+++ Joins/Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linq
+{
+  public  class Address
+    {
+        public int ID { get; set; }
+        public string AddressLine { get; set; }
+        public static List<Address> GetAllAddresses()
+        {
+            return new List<Address>()
+            {
+                new Address { ID = 1, AddressLine = "AddressLine1"},
+                new Address { ID = 2, AddressLine = "AddressLine2"},
+                new Address { ID = 3, AddressLine = "AddressLine3"},
+                new Address { ID = 4, AddressLine = "AddressLine4"},
+                new Address { ID = 5, AddressLine = "AddressLine5"},
+                new Address { ID = 9, AddressLine = "AddressLine9"},
+                new Address { ID = 10, AddressLine = "AddressLine10"},
+                new Address { ID = 11, AddressLine = "AddressLine11"},
+            };
+        }
+    }
+}
